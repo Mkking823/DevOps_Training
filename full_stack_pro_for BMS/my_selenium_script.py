d48cc35393bddcaac4db42a3c0a9c85e14323461
Mkking823 <121536739+Mkking823@users.noreply.github.com>
2023-11-23 09:32:53 +0530

    Update my_selenium_script.py

--- full_stack_pro_for BMS/my_selenium_script.py
+++ full_stack_pro_for BMS/my_selenium_script.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-#here
-# Rest of your code ...
 
 # Function to wait for an element to be clickable
 def wait_for_element_to_be_clickable(by, value, timeout=10):
